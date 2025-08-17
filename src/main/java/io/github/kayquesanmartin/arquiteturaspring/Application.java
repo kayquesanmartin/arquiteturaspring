f04d406eba4c994b3996db44bf824a9122094d50
kayquesanmartin <kayque.sanmartin65@gmail.com>
2025-08-17 12:52:20 -0300

    feat: change de Application class to learn about context and builder

--- src/main/java/io/github/kayquesanmartin/arquiteturaspring/Application.java
+++ src/main/java/io/github/kayquesanmartin/arquiteturaspring/Application.java
@@ -1,13 +1,36 @@
 package io.github.kayquesanmartin.arquiteturaspring;
 
-import org.springframework.boot.SpringApplication;
+import org.springframework.boot.Banner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
 
-@SpringBootApplication
+@SpringBootApplication // Importante para o contexto de execução da aplicação
 public class Application {
 
+	// Só mexemos quando é realmente necessário.
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		// SpringApplication.run(Application.class, args);
+
+		// Importante passar o Application.class
+		SpringApplicationBuilder builder = new SpringApplicationBuilder(Application.class);
+		
+		builder.bannerMode(Banner.Mode.OFF);
+		builder.profiles("producao");
+
+		builder.run(args); // Args são importantes para nossa aplicação	
+
+		// Contexto da aplicação já iniciada
+		ConfigurableApplicationContext applicationContext = builder.context();
+		// var userRepository = applicationContext.getBean("userRepository");
+
+		ConfigurableEnvironment environment = applicationContext.getEnvironment();
+		String applicationName = environment.getProperty("spring.application.name");
+		System.out.println(applicationName);
+		
+		// builder.properties("spring.datasource.url=jdbc://");
+
 	}
 
 }
